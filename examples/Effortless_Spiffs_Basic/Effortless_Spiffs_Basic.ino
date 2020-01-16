1de9aca52350038e2de565ada7891951269ff76e
thebigpotatoe <17895072+thebigpotatoe@users.noreply.github.com>
2020-01-16 19:26:42 +1100

    Added copywrite notice to sketch

--- examples/Effortless_Spiffs_Basic/Effortless_Spiffs_Basic.ino
+++ examples/Effortless_Spiffs_Basic/Effortless_Spiffs_Basic.ino
@@ -1,3 +1,17 @@
+/*
+Copyright (c) 2019 thebigpotatoe
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+*/
+
 // #include <ArduinoJson.h>  // Uncomment this to include ArduinoJson. Make sure to have it installed first
 #include <C:\Users\Mitch\Google Drive\Github\Effortless-SPIFFS\Effortless_SPIFFS.h>
 
